15166a9bf2c06381c2dd8e4084204bf166328f1d
yusufeko65 <yusufekon65@gmail.com>
2024-03-22 21:45:09 +0700

    refactor : delay 5s

--- application/controllers/Moota.php
+++ application/controllers/Moota.php
@@ -116,7 +116,7 @@ class Moota extends CI_Controller {
             foreach($totals as $ky => $vl){
                 $amount = $vl['total'];
 
-                sleep(4);
+                sleep(5);
 
                 $mutations = $this->mutation_search_amount($token,$bank_id,$amount);
                 if(isset($mutations['mutation'])){
@@ -124,12 +124,12 @@ class Moota extends CI_Controller {
                         $this->pembayaran->update_order($vl,$mutations['mutation'][0]);
                     }
                 }else{
-                    echo '------- ' . date('Y-m-d H:i:s') . '<br>';
-                    echo 'Bank ID : ' . $val['bank_type'] . '<br>';
-                    echo 'Amount  : ' . $amount . '<br>';
-                    echo 'msg     : Lost Conection' . '<br>';
-                    echo '-------------------------' . '<br>';
-                    echo '' . '<br>';
+                    echo '------- ' . date('Y-m-d H:i:s') . '\r\n';
+                    echo 'Bank ID : ' . $val['bank_type'] . '\r\n';
+                    echo 'Amount  : ' . $amount . '\r\n';
+                    echo 'msg     : Lost Conection' . '\r\n';
+                    echo '-------------------------' . '\r\n';
+                    echo '' . '\r\n';
                 }
             }
         }
