1613ddc15648ac6644c8bb810a577c12e6d7ecf2
yusufeko65 <yusufekon65@gmail.com>
2024-03-29 00:00:35 +0700

    fix : delete check booking

--- application/models/Pembayaran.php
+++ application/models/Pembayaran.php
@@ -39,20 +39,6 @@ class Pembayaran extends CI_Model
                 $this->update_order($v,$value);
                 $status = true;
                 break;
-            }else{
-                // check masa booking
-                $masa = $this->check_masaBooking() ;
-                $masa = isset($masa[0]) ? (int) $masa[0]['setting_value'] : 24;
-
-                $now = date('Y-m-d H:i:s');
-                $pesan = $v['pesanan_tgl'];
-
-                $snow = strtotime($now);
-                $spesan = strtotime($pesan);
-                $selisih = ceil( ($snow - $spesan) / (60 * 60));
-                if($selisih > $masa){
-                   // $this->update_order_cancel($v['pesanan_no']);
-                }
             }
         }
 
@@ -97,89 +83,5 @@ class Pembayaran extends CI_Model
         }
 
         return true;
-    }
-
-    public function update_order_cancel($no_order=0){
-        // Update status
-        $this->db->where('pesanan_no',$no_order);
-        $this->db->update('_order',[
-            'status_id' => 14
-        ]);
-
-        // Insert History Order Status
-        $this->db->insert('_order_status',[
-            'nopesanan'     => $no_order,
-            'tanggal'       => date('Y-m-d H:i:s'),
-            'keterangan'    => 'Batal otomatis',
-            'status_id'     => 14
-        ]);
-    }
-	public function get_status_cancel(){
-		$sql = "SELECT `idostatus`,`nopesanan` FROM `_order_status` WHERE `status_id`=14 AND `keterangan`='Batal Otomatis'";
-	
-		$query = $this->db->query($sql);
-	    return $query->result_array();
-	}
-		
-	public function get_order_cancel(){
-		$cancels = [];
-		$status = $this->get_status_cancel();
-		foreach($status as $key => $val){
-			$ids = $val['idostatus'];
-			$cancels[] = $val['nopesanan'];
-
-			// Update
-			$this->db->where('idostatus',$ids);
-            $this->db->update('_order_status',[
-                'keterangan' => '#Batal Otomatis'
-            ]);
-		}
-		
-		$cancel = implode(',',$cancels);
-        $sql = "SELECT * FROM `_order_detail` WHERE `pesanan_no` IN ($cancel)";
-
-        $query = $this->db->query($sql);
-        return $query->result_array();
-    }
-
-	public function update_stok_cancel(){
-		$cancel = $this->get_order_cancel();
-		foreach($cancel as $key => $val){
-			$qty = $val['jml'];
-			$idproduk = $val['produk_id'];
-			$idukuran = $val['ukuranid'];
-			$idwarna = $val['warnaid'];
-
-			// Get Produk
-			$sql = "SELECT idopt,stok FROM `_produk_options` WHERE `idproduk`='$idproduk' AND ukuran='$idukuran' AND warna='$idwarna'";
-
-	        $query = $this->db->query($sql);
-	        $produk = $query->result_array();
-
-			if(isset($produk[0])){
-				$idopt = $produk[0]['idopt'];
-				$stok = $produk[0]['stok'] + $qty;
-
-				// Update Stock
-				$this->db->where('idopt',$idopt);
-	            $this->db->update('_produk_options',[
-	                'stok' => $stok
-	            ]);
-
-				// Update Stock Produk
-				$sqls = "SELECT jml_stok FROM `_produk` WHERE `idproduk`='$idproduk'";
-
-		        $querys = $this->db->query($sqls);
-		        $prd = $querys->result_array();
-
-				$jstok = $prd[0]['jml_stok'] + $qty;
-				
-				$this->db->where('idproduk',$idproduk);
-	            $this->db->update('_produk',[
-	                'jml_stok' => $jstok
-	            ]);
-			}
-			
-		}
     }
 }
